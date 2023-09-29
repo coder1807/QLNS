7e8541c3c0f0a85fe0015e9b7619a0cec25dc2dd
Coder1807 <bhviet.910lhphong.cr@gmail.com>
2023-09-29 15:39:13 +0700

    first commit

---
+++ SQLNhanSu.sql
@@ -0,0 +1,38 @@
+﻿create database QLNhansu
+go
+
+use QLNhansu
+go
+
+create table Phongban (
+	MaPB char(2),
+	TenPB nvarchar(30),
+	primary key (MaPB),
+)
+
+create table Nhanvien(
+	MaNV char(6),
+	TenNV nvarchar(20),
+	Ngaysinh datetime,
+	MaPB char(2),
+	primary key (MaNV)
+)
+
+alter table Nhanvien
+	add constraint fk_MaPB
+	foreign key (MaPB)
+	references Phongban(MaPB)
+	go
+
+insert into Phongban (MaPB,TenPB)
+values ('1',N'Phòng ban Sale')
+insert into Phongban (MaPB,TenPB)
+values ('2',N'Phòng ban IT')
+
+insert into Nhanvien (MaNV,TenNV,Ngaysinh,MaPB)
+values ('111',N'Nguyễn Văn A','2005-02-20','1')
+insert into Nhanvien (MaNV,TenNV,Ngaysinh,MaPB)
+values ('222',N'Nguyễn Văn B','2001-10-15','2')
+insert into Nhanvien (MaNV,TenNV,Ngaysinh,MaPB)
+values ('333',N'Nguyễn Văn C','2000-10-11','2')
+
